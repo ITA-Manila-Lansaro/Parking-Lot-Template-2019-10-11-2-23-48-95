0a5be843bacd2c00a0c981627352e3460bfd69d4
robertnajiblansangan <robert.najib.lansangan@oocl.com>
2019-10-21 02:07:57 +0800

    Added unit tests

---
+++ src/test/java/com/thoughtworks/parking_lot/Controller/ParkingLotControllerTest.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.parking_lot.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingLotControllerTest {
+
+    @Test
+    void addParkingLot() {
+    }
+
+    @Test
+    void getAllParkingLot() {
+    }
+
+    @Test
+    void getParkingLot() {
+    }
+
+    @Test
+    void updateCapacity() {
+    }
+}
---
+++ src/test/java/com/thoughtworks/parking_lot/Controller/ParkingOrderControllerTest.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.parking_lot.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingOrderControllerTest {
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void checkOut() {
+    }
+}
